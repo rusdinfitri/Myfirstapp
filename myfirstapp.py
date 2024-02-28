898a6f793af5477c843c990d6a532a6d7422103a
rusdinfitri <161561122+rusdinfitri@users.noreply.github.com>
2024-02-28 16:19:36 +0800

    Update myfirstapp.py

--- myfirstapp.py
+++ myfirstapp.py
@@ -1,3 +1,4 @@
+
 %%writefile Advertising.py
 import streamlit as st
 import pandas as pd
@@ -12,9 +13,9 @@ This app predicts the **Sales** type!
 st.sidebar.header('User Input Parameters')
 
 def user_input_features():
-    TV= st.sidebar.slider('TV', 0.7, 297, 100)
-    Radio = st.sidebar.slider('Radio', 0, 50, 15)
-    Newspaper = st.sidebar.slider('Newspaper', 0.3, 114, 20)
+    TV= st.sidebar.slider('TV', 0.7, 297.0, 100) #all Float
+    Radio = st.sidebar.slider('Radio', 0, 50.0, 15)
+    Newspaper = st.sidebar.slider('Newspaper', 0.3, 114.0, 20)
     
     data = {'TV': TV,
             'Radio': Radio,
