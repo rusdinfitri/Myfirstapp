709b3f72cc4efaa58a8305f0ec4247919c344429
rusdinfitri <161561122+rusdinfitri@users.noreply.github.com>
2024-02-28 16:24:13 +0800

    Update myfirstapp.py

--- myfirstapp.py
+++ myfirstapp.py
@@ -6,7 +6,6 @@ import pickle
 st.write("""
 #Advertising Sales Prediction App
 
-
 This app predicts the **Sales** type!
 """)
 
@@ -16,9 +15,8 @@ def user_input_features():
     TV= st.sidebar.slider('TV', 0.7, 297.0, 100) 
     Radio = st.sidebar.slider('Radio', 0, 50.0, 15)
     Newspaper = st.sidebar.slider('Newspaper', 0.3, 114.0, 20)
-    
-    data = {'TV': TV,
-            'Radio': Radio,
+    data = {'TV': tv,
+            'Radio': radio,
             'Newspaper': Newspaper,}
             
     features = pd.DataFrame(data, index=[0])
