9fcbf2bbb54e478cfe1795a5fefb6960a7b6774b
rusdinfitri <161561122+rusdinfitri@users.noreply.github.com>
2024-02-28 15:58:51 +0800

    Update myfirstapp.py

--- myfirstapp.py
+++ myfirstapp.py
@@ -1,48 +1,36 @@
+%%writefile Advertising.py
 import streamlit as st
-import numpy as np
 import pandas as pd
-import time
+import pickle
 
-st.header("My first Streamlit App")
+st.write("""
+# Sales Prediction App
 
-option = st.sidebar.selectbox(
-    'Select a mini project',
-     ['line chart','map','T n C','Long Process'])
+This app predicts the **Sales** type!
+""")
 
-if option=='line chart':
-    chart_data = pd.DataFrame(
-    np.random.randn(20, 3),
-    columns=['a', 'b', 'c'])
+st.sidebar.header('User Input Parameters')
 
-    st.line_chart(chart_data)
+def user_input_features():
+    TV= st.sidebar.slider('TV', 0.7, 297, 100)
+    Radio = st.sidebar.slider('Radio', 0, 50, 15)
+    Newspaper = st.sidebar.slider('Newspaper', 0.3, 114, 20)
+    
+    data = {'TV': TV,
+            'Radio': Radio,
+            'Newspaper': Newspaper,}
+            
+    features = pd.DataFrame(data, index=[0])
+    return features
 
-elif option=='map':
-    map_data = pd.DataFrame(
-    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
-    columns=['lat', 'lon'])
+df = user_input_features()
 
-    st.map(map_data)
+st.subheader('User Input parameters')
+st.write(df)
 
-elif option=='T n C':
-    st.write('Before you continue, please read the [terms and conditions](https://www.gnu.org/licenses/gpl-3.0.en.html)')
-    show = st.checkbox('I agree the terms and conditions')
-    if show:
-        st.write(pd.DataFrame({
-        'Intplan': ['yes', 'yes', 'yes', 'no'],
-        'Churn Status': [0, 0, 0, 1]
-        }))
+loaded_model = pickle.load(open("Advertisingmodel.h5", "rb"))
 
-else:
-    'Starting a long computation...'
+prediction = loaded_model.predict(df)
 
-    latest_iteration = st.empty()
-    bar = st.progress(0)
-
-    for i in range(100):
-        latest_iteration.text(f'Iteration {i+1}')
-        bar.progress(i + 1)
-        time.sleep(0.1)
-
-    '...and now we\'re done!'
-
-   
+st.subheader('Prediction')
+st.write(prediction)
